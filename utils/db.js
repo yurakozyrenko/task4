d2c39fe37f0d2ecf50b50e0a6c6fba0563aedfcd
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2023-09-13 18:15:02 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,12 +4,17 @@ async function startDb() {
     await MongoClient.connect(
         'mongodb+srv://yurikozyrenko:N1357=Dt@cluster0.6xqtrex.mongodb.net/ToDo?retryWrites=true&w=majority',
         {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            },
+        },
+        (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        }
     );
 }
 
