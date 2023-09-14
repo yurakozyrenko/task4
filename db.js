2dee76bd1152d13070955e418d00b48a2851b2b7
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2023-09-14 21:37:03 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 const { MongoClient } = require('mongodb');
-const URL =
-    'mongodb+srv://yurikozyrenko:N1357=Dt@cluster0.6xqtrex.mongodb.net/ToDo?retryWrites=true&w=majority';
-
-const mongoClient = new MongoClient(URL);
+require('dotenv').config();
+const mongoClient = new MongoClient(process.env.URL);
 
 let dbConnection;
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect(URL)
+        MongoClient.connect(process.env.URL)
             .then((client) => {
                 console.log('Connection to MongoDB');
                 dbConnection = client.db();
